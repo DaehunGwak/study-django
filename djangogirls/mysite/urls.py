bbb53ae0755d900eae84baf7188136a5a749d137
DaehunGwak <rhkrgns56@naver.com>
2020-03-19 17:50:21 +0900

    [UPDATE] 프로젝트 구조 변경

--- djangogirls/mysite/urls.py
+++ djangogirls/mysite/urls.py
@@ -1,4 +1,4 @@
-"""djangogirls URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
