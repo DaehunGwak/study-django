4fd5ac22418eb3852f502d527c9fcdd3592e4d94
DaehunGwak <rhkrgns56@naver.com>
2020-03-24 13:17:19 +0900

    [ADD] djangogirls: detail page

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -13,3 +13,11 @@ def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     logger.info(f"posts: {posts}")
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    logger.info(f"request: {request}")
+    post = get_object_or_404(Post, pk=pk)
+    logger.info(f"post: {post}")
+    return render(request, 'blog/post_detail.html', {'post': post})
+
