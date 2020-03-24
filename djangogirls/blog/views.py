abc52c8dae15d60133490bb49b77b7bdb2acf89b
DaehunGwak <rhkrgns56@naver.com>
2020-03-24 12:14:53 +0900

    [UPDATE] djagogirls: model-view 동적 템플릿

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,5 +1,8 @@
 
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 import logging
 
 logger = logging.getLogger(__name__)
@@ -7,4 +10,6 @@ logger = logging.getLogger(__name__)
 
 def post_list(request):
     logger.info(f"request: {request}")
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    logger.info(f"posts: {posts}")
+    return render(request, 'blog/post_list.html', {'posts': posts})
