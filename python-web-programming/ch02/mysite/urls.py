da375d5a18d4dfff573fd21c7710f0c947214023
DaehunGwak <rhkrgns56@naver.com>
2020-04-11 17:22:35 +0900

    Add generic views and url mapping

--- python-web-programming/ch02/mysite/urls.py
+++ python-web-programming/ch02/mysite/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bookmark.views import BookmarkLV, BookmarkDV
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # class-based views
+    path('bookmark', BookmarkLV.as_view(), name='index'),
+    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ]
