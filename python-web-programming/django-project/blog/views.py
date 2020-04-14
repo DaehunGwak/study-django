926cea75093e52bfffdb80046a1fbf378a8b65b6
DaehunGwak <rhkrgns56@naver.com>
2020-04-14 10:33:37 +0900

    Edit urls files & Add blog views

--- python-web-programming/django-project/blog/views.py
+++ python-web-programming/django-project/blog/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+class PostDV(DetailView):
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
--- python-web-programming/django-project/mysite/urls.py
+++ python-web-programming/django-project/mysite/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from bookmark.views import BookmarkLV, BookmarkDV
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # class-based views
-    path('bookmark', BookmarkLV.as_view(), name='index'),
-    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
 ]
